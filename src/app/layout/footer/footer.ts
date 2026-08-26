dd2015d55fed51b7393dda14e4da6dc30dcac196
nataliapianca <88843341+nataliapianca@users.noreply.github.com>
2026-08-26 20:57:31 -0300

    feat: criar navbar e footer responsivos

---
+++ src/app/layout/footer/footer.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer', standalone: true,
+  templateUrl: './footer.html', styleUrl: './footer.scss',
+})
+export class Footer {}
